2f5c716d0f72f72bdbbac85e1ac5b8bc47fdec5d
Jonathan Sudlow <jsudlow@gmail.com>
2013-07-01 22:55:21 -0500

    made new player class and divided functionality

--- ex1.1.py
+++ ex1.1.py
@@ -14,44 +14,64 @@ class Application:
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.screen = pygame.display.set_mode([self.screen_width,self.screen_height])
+        self.player = Player(400,400,40,BLACK)
             
     def setCaption(self,caption):
         pygame.display.set_caption(caption)
+
     def draw_screen(self):
         self.screen.fill(WHITE)
-        pygame.draw.circle(self.screen, BLACK, [self.circle_position[0],self.circle_position[1]], 40)
+        self.player.draw(self.screen)
         pygame.display.flip()
-    def move_down(self,ammount):
-        self.circle_position[1] += ammount
-    def move_up(self,ammount):
-        self.circle_position[1] -= ammount
-    def move_right(self,ammount):
-        self.circle_position[0] += ammount
-    def move_left(self,ammount): 
-        self.circle_position[0] -= ammount
+    
     def handle_events(self):
         for event in pygame.event.get(): # User did something
-                if event.type == pygame.QUIT: # If user clicked close
-                    self.done=True # Flag that we are done so we exit this loop
-                if event.type == KEYDOWN:
-                    if event.key == K_DOWN:
-                        self.move_down(10)
-                    if event.key == K_UP:
-                        self.move_up(10)
-                    if event.key == K_RIGHT:
-                        self.move_right(10)
-                    if event.key == K_LEFT:
-                        self.move_left(10)
+            if event.type == pygame.QUIT: # If user clicked close
+                self.done=True # Flag that we are done so we exit this loop
+            self.player.handle_event(event)
+
     def run(self):
         self.done = False
         clock = pygame.time.Clock()
-        self.circle_position = [60,250] 
-        
+              
         while not self.done:
             clock.tick(10)
-            self.handle_events()
+            self.handle_events()     
             self.draw_screen()
               
+
+class Player:
+    def __init__(self,x,y,radius,color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        
+    def move_down(self,ammount):
+        self.y += ammount
+
+    def move_up(self,ammount):
+        self.y -= ammount
+
+    def move_right(self,ammount):
+        self.x += ammount
+
+    def move_left(self,ammount): 
+        self.x -= ammount
+
+    def handle_event(self,event):
+        if event.type == KEYDOWN:
+            if event.key == K_DOWN:
+                self.move_down(10)
+            if event.key == K_UP:
+                self.move_up(10)
+            if event.key == K_RIGHT:
+                self.move_right(10)
+            if event.key == K_LEFT:
+                self.move_left(10)
+                
+    def draw(self,screen):
+        pygame.draw.circle(screen, BLACK, [self.x,self.y], self.radius)
 
 app = Application(600,600)
 app.setCaption("Example py1.1")
