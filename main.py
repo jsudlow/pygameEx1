37200cc8df7375e14a956d80716a00877293323a
Dustin Lacewell <dlacewell@gmail.com>
2013-07-08 22:15:48 -0500

    Default sprites and a bit of clean up

--- main.py
+++ main.py
@@ -10,6 +10,98 @@ BLUE =  (  0,   0, 255)
 GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
 
+class Sprite(object):
+    def __init__(self, color):
+        self.color = color
+
+class CircleSprite(Sprite):
+    def __init__(self, radius, color):
+        super(CircleSprite, self).__init__(color)
+        self.radius = radius
+    def draw(self, screen, x, y):  
+        pygame.draw.circle(
+            screen, 
+            self.color, 
+            [x,y], 
+            self.radius,
+        )
+
+class SquareSprite(Sprite):
+    def __init__(self,side_length,color):
+        super(SquareSprite,self).__init__(color)
+        self.side_length = side_length
+    def draw(self,screen,x,y):
+        pygame.draw.rect(
+            screen, 
+            self.color, 
+            [x, y, self.side_length, self.side_length], 
+            2,
+        )
+    
+
+class BaseEntity(object):
+    
+    default_sprite = CircleSprite(40, BLACK)
+
+    def __init__(self, app, x, y, sprite=None):
+        self.app = app
+        self.x = x
+        self.y = y      
+
+        if sprite is None:
+            sprite = self.default_sprite
+
+        self.sprite = sprite
+
+    def move_down(self, amount):
+        self.y += amount
+
+    def move_up(self, amount):
+        self.y -= amount
+
+    def move_right(self, amount):
+        self.x += amount
+
+    def move_left(self, amount): 
+        self.x -= amount
+
+    def update(self, dt, time): pass
+
+    def draw(self,screen):
+        self.sprite.draw(screen,self.x, self.y)
+
+
+class Player(BaseEntity):
+
+    default_sprite = CircleSprite(40, BLACK)
+
+    def handle_event(self,event):
+        if event.type == KEYDOWN:
+            if event.key == K_DOWN:
+                self.move_down(10)
+            if event.key == K_UP:
+                self.move_up(10)
+            if event.key == K_RIGHT:
+                self.move_right(10)
+            if event.key == K_LEFT:
+                self.move_left(10)
+
+
+class Enemy(BaseEntity):
+
+    default_sprite = SquareSprite(40, GREEN)
+        
+    def update(self, dt, time):
+        if self.x > self.app.player.x:
+            self.move_left(10)
+        else:
+            self.move_right(10)
+        if self.y > self.app.player.y:
+            self.move_up(10)
+        else:
+            self.move_down(10)
+        
+
 class Application:
     def __init__(self, screen_width, screen_height):
         self.screen_width = screen_width
@@ -20,19 +112,17 @@ class Application:
         ])
         
         self.player = Player(
-            200, 200,
-            CircleEntity(40, BLACK),
+            self, 200, 200,
         )
 
-        self.enemey = Enemey(
-            400,400,
-            SquareEntity(40,GREEN),
-            self
-            )
-        
         self.entities = []
-        self.entities.append(self.enemey)
+        self.spawn_enemy(400, 400)
             
+    def spawn_enemy(self, x, y):
+        self.entities.append(
+            Enemy(self, x, y)
+        )
+
     def setCaption(self,caption):
         pygame.display.set_caption(caption)
 
@@ -73,88 +163,7 @@ class Application:
             self.handle_events()     
             self.update_entities(dt, time)
             self.draw_screen()
-              
-class BaseEntity(object):
-    def __init__(self, x, y, sprite):
-        self.x = x
-        self.y = y      
-        self.sprite = sprite
 
-    def move_down(self, amount):
-        self.y += amount
-
-    def move_up(self, amount):
-        self.y -= amount
-
-    def move_right(self, amount):
-        self.x += amount
-
-    def move_left(self, amount): 
-        self.x -= amount
-
-    def update(self, dt, time): pass
-
-    def draw(self,screen):
-        self.sprite.draw(screen,self.x, self.y)
-
-class Player(BaseEntity):
-    def __init__(self,sprite,x,y):
-        super(Player,self).__init__(sprite,x,y)
-        
-    def handle_event(self,event):
-        if event.type == KEYDOWN:
-            if event.key == K_DOWN:
-                self.move_down(10)
-            if event.key == K_UP:
-                self.move_up(10)
-            if event.key == K_RIGHT:
-                self.move_right(10)
-            if event.key == K_LEFT:
-                self.move_left(10)
-
-class Enemey(BaseEntity):
-    def __init__(self,sprite,x,y,app):
-        super(Enemey,self).__init__(sprite,x,y)
-        self.app = app
-        
-    def update(self,dt,time):
-        if self.x > self.app.player.x:
-            self.move_left(10)
-        else:
-            self.move_right(10)
-        if self.y > self.app.player.y:
-            self.move_up(10)
-        else:
-            self.move_down(10)
-        
-class Sprite(object):
-    def __init__(self, color):
-        self.color = color
-
-class CircleEntity(Sprite):
-    def __init__(self, radius, color):
-        super(CircleEntity, self).__init__(color)
-        self.radius = radius
-    def draw(self, screen, x, y):  
-        pygame.draw.circle(
-            screen, 
-            self.color, 
-            [x,y], 
-            self.radius,
-        )
-
-class SquareEntity(Sprite):
-    def __init__(self,side_length,color):
-        super(SquareEntity,self).__init__(color)
-        self.side_length = side_length
-    def draw(self,screen,x,y):
-        pygame.draw.rect(
-            screen, 
-            self.color, 
-            [x, y, self.side_length, self.side_length], 
-            2,
-        )
-    
 
 app = Application(600,600)
 app.setCaption("Example main.py")
